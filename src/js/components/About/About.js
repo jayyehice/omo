c6cfc2b8ee6cb845aebcc311867979b0da0d8abe
jayyehice <93643298+jayyehice@users.noreply.github.com>
2023-01-31 09:56:58 +0800

    edit about page
    * add OMOIntroduction.js
    
    * add mdi icon
    
    * add q&a question and usb jpg

--- src/js/components/About/About.js
+++ src/js/components/About/About.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import Faq from './Faq';
+import OMOIntroduction from './OMOIntroduction';
+
 const About = (props) => {
     return (
         <div className="mx-auto w-8/12 mt-5">
-            <Faq />
+            <OMOIntroduction className="p-2"/>
+            <Faq className="p-2"/>
         </div>
     );
 };
