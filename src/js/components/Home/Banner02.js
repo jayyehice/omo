05a6fadfff2a9da29b099f1fe0468c7af59789c5
jay <jayyehice@gmail.com>
2023-02-08 14:54:08 +0800

    add news.js

--- src/js/components/Home/Banner02.js
+++ src/js/components/Home/Banner02.js
@@ -5,7 +5,7 @@ const Banner02 = (props) => {
         <section className="mx-auto w-10/12 my-6 bg-auto bg-no-repeat bg-center h-96" style={{ backgroundImage: `url("/static/img/banner/banner2.jpg")` }}>
             <div className="w-5/12 ml-16 mt-20 flex items-center justify-between">
                 <h3 className="text-5xl bg-black rounded-[10px] text-white text-center p-3 y-space-0">排版、製作成品</h3>
-                <span class="mdi mdi-arrow-right-bold text-7xl"></span>
+                <span className="mdi mdi-arrow-right-bold text-7xl"></span>
                 <h3 className="text-5xl rounded-[10px] border-4 border-black text-center p-3">一次到位</h3>
             </div>
         </section>
--- src/js/components/Home/News.js
+++ src/js/components/Home/News.js
@@ -1,31 +1,35 @@
 import React, { Component } from "react";
 
 const News = (props) => {
+    let news_list = [
+        { content: '[新品上市]  客製化小夜燈-開始販售', date: '2023.03.01' },
+        { content: '[促銷活動] 64GB 隨身碟買五送一', date: '2023.02.10' },
+    ];
+
+    const newsList = news_list.map((obj, index) => {
+        return (
+            <tr key={index}>
+                <td className="w-1/3 text-left py-3 px-4">{obj.content}</td>
+                <td className="w-1/3 text-left py-3 px-4">{obj.date}</td>
+            </tr>
+        )
+    });
     return (
         <div className="bg-blue-50 py-5">
-            <section className="mx-auto w-10/12">
-                <div className="flex items-center justify-center">
-                    <div className="subtitle">
-                        <h2>最新消息</h2>
-                    </div>
+            <section className="mx-auto w-10/12 pt-6">
+                <h2 className="w-full text-4xl font-bold text-center"><span className="mdi mdi-information text-blue-600"></span>最新消息</h2>
 
-                    <div className="news_table">
-                        <table>
-                            <thead>
+                <div className="md:px-32 py-8 w-full">
+                    <div className="shadow overflow-hidden rounded border-b border-gray-200">
+                        <table className="min-w-full bg-white">
+                            <thead className="bg-gray-800 text-white">
                                 <tr>
-                                    <td>標題</td>
-                                    <td>日期</td>
+                                    <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">標題</th>
+                                    <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">日期</th>
                                 </tr>
                             </thead>
-                            <tbody>
-                                <tr>
-                                    <td>[新品上市]  客製化小夜燈-開始販售</td>
-                                    <td>2022.03.24</td>
-                                </tr>
-                                <tr>
-                                    <td>[促銷活動] 64GB 隨身碟買五送一 </td>
-                                    <td>2022.03.17</td>
-                                </tr>
+                            <tbody className="text-gray-700">
+                                {newsList}
                             </tbody>
                         </table>
                     </div>
